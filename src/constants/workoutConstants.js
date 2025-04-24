67af64708f2442fcdcfc68df16528a1715bab15a
ang <alex.ng13@gmail.com>
2025-04-23 21:20:37 -0400

    added more sports to list

---
+++ src/constants/workoutConstants.js
@@ -0,0 +1,33 @@
+export const activityTypes = ['Sports', 'Run', 'Weight Lifting', 'Conditioning', 'Recovery'];
+
+export const sportOptions = [
+    'Basketball', 'Soccer', 'Track', 'Volleyball', 'Baseball', 'Lacrosse', 'Ice Hockey',
+    'Football', 'Tennis', 'Swimming', 'Golf', 'Wrestling', 'Softball', 'CrossFit', 'Cycling', 'Rowing'
+];
+
+export const sportSubtypes = {
+    Basketball: ['Practice', 'Game'],
+    Soccer: ['Practice', 'Game'],
+    Track: ['Sprint', 'Distance', 'Meet'],
+    Volleyball: ['Practice', 'Game'],
+    Baseball: ['Practice', 'Game', 'Bullpen'],
+    Lacrosse: ['Practice', 'Game'],
+    Ice Hockey: ['Practice', 'Game'],
+    Football: ['Practice', 'Game', 'Scrimmage'],
+    Tennis: ['Practice', 'Match'],
+    Swimming: ['Practice', 'Meet'],
+    Golf: ['Practice', 'Tournament'],
+    Wrestling: ['Practice', 'Match'],
+    Softball: ['Practice', 'Game'],
+    CrossFit: ['WOD', 'Competition'],
+    Cycling: ['Ride', 'Race'],
+    Rowing: ['Practice', 'Race']
+};
+
+export const runTypes = ['Distance', 'Sprints', 'Intervals', 'Hills', 'Tempo', 'Recovery'];
+
+export const recoveryOptions = ['Ice Bath', 'Stretching', 'Foam Rolling', 'Massage', 'Rest Day'];
+
+export const conditioningOptions = ['Agility Drills', 'Plyometrics', 'HIIT', 'Circuit Training'];
+
+export const muscleGroups = ['Chest', 'Back', 'Legs', 'Arms', 'Shoulders', 'Core'];
