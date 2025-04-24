749075d761e3c49f48170f42dd0c652f52db2201
ang <alex.ng13@gmail.com>
2025-04-23 21:40:54 -0400

    added more sports to list

--- src/pages/WorkoutsPage.js
+++ src/pages/WorkoutsPage.js
@@ -54,7 +54,7 @@ const WorkoutPage = () => {
                             href="/"
                             className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold px-4 py-2 rounded shadow"
                         >
-                            Back Home
+                            Home
                         </a>
                     </div>
                 </div>
