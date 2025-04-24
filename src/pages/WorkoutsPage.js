6d30a853176c64315d538b120d520d76dec2744b
ang <alex.ng13@gmail.com>
2025-04-24 12:13:06 -0400

    moved  sports entris table to components

--- src/pages/WorkoutsPage.js
+++ src/pages/WorkoutsPage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { FaDumbbell } from 'react-icons/fa';
 import WorkoutFormModal from '../components/Workout/WorkoutFormModal';
 import WorkoutTableSection from '../components/Workout/WorkoutTableSection';
 import FitnessSnapshot from '../components/Workout/FitnessSnapshot';
@@ -35,29 +34,6 @@ const WorkoutPage = () => {
     return (
         <div className="min-h-screen bg-gradient-to-br from-white to-indigo-50 dark:from-gray-900 dark:to-gray-800 p-6 text-gray-900 dark:text-white">
             <div className="max-w-4xl mx-auto">
-                <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start mb-6">
-                    <div className="flex flex-col items-center sm:items-start sm:w-1/2">
-                        <FaDumbbell className="text-indigo-600 dark:text-indigo-300 text-4xl mb-1" />
-                        <h2 className="text-xl font-bold text-center sm:text-left leading-tight">
-                            Workout<br />Tracker
-                        </h2>
-                    </div>
-                    <div className="flex flex-col sm:flex-col items-center sm:items-end space-y-2 mt-4 sm:mt-0">
-                        <button
-                            onClick={() => setShowModal(true)}
-                            className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-4 py-2 rounded shadow w-40 text-sm sm:text-base"
-                        >
-                            Add Workout
-                        </button>
-                        <button
-                            onClick={() => window.location.href = '/'}
-                            className="bg-gray-500 hover:bg-gray-600 text-white font-semibold px-4 py-2 rounded shadow w-40 text-sm sm:text-base"
-                        >
-                            Home
-                        </button>
-                    </div>
-                </div>
-
                 <FitnessSnapshot workouts={workouts} />
 
                 {Object.keys(groupedWorkouts).map((type) => (
