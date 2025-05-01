27aa1014e6409d1c3622eda6b01e4249fb97bb34
ang <alex.ng13@gmail.com>
2025-05-01 14:21:56 -0400

    API setup

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // ✅ Replace with your actual project values from https://app.supabase.com/project/_/settings/api
-const SUPABASE_URL = 'https://your-project-id.supabase.co';
-const SUPABASE_ANON_KEY = 'your-anon-public-api-key';
+const SUPABASE_URL = 'https://zaujjqbschyhldkmwyru.supabase.co' ;
+const SUPABASE_ANON_KEY = '***REMOVED***.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphdWpqcWJzY2h5aGxka213eXJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTc3MzksImV4cCI6MjA2MTY3MzczOX0.tgfWScFigNyFrfNVDuCoW2jHBCEgGG0Y15EC4nSluMI' ;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
