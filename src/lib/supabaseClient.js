a0f85fc0687768e684911ce460eb43d99629c35b
ang <alex.ng13@gmail.com>
2025-05-02 16:21:27 -0400

    VITE changes

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,7 @@
-// src/lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// ✅ Replace with your actual project values from https://app.supabase.com/project/_/settings/api
-const SUPABASE_URL = 'https://iqdkxgfyyakwmhyyahfg.supabase.co' ;
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxZGt4Z2Z5eWFrd21oeXlhaGZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxODQxMzIsImV4cCI6MjA2MTc2MDEzMn0.Vyny_4dB-7q2vJpxtwQdlBUIotauNWdkzU_n-3i5ze0' ;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
