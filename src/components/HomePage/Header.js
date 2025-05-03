48ce29b1e78f8ee13a71cdea2a2864d12723d921
ang <alex.ng13@gmail.com>
2025-05-02 21:18:48 -0400

    New design HomePage.js - production

---
+++ src/components/HomePage/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="text-sm text-center text-gray-500 dark:text-gray-400 mb-4">
+            processwins.app
+        </div>
+    );
+}
