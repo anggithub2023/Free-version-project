704378c97f246c5843646fd49ef9ebd2419ee985
ang <alex.ng13@gmail.com>
2025-05-05 09:41:56 -0400

    CTA bar resusble

---
+++ src/components/StickyCtaBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    MdFileDownload,
+    MdDeleteForever,
+    MdHome,
+    MdInsights
+} from 'react-icons/md';
+
+export default function StickyCtaBar({ onDownload, onClear, onInsights, onHome }) {
+    return (
+        <div className="fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-900 border-t dark:border-gray-700 shadow-md px-4 py-3 flex justify-around items-center z-50">
+            <button onClick={onDownload} className="flex flex-col items-center text-xs text-gray-700 dark:text-gray-200 hover:text-indigo-600">
+                <MdFileDownload size={24} />
+                <span>Download</span>
+            </button>
+            <button onClick={onClear} className="flex flex-col items-center text-xs text-gray-700 dark:text-gray-200 hover:text-yellow-600">
+                <MdDeleteForever size={24} />
+                <span>Clear</span>
+            </button>
+            <button onClick={onInsights} className="flex flex-col items-center text-xs text-gray-700 dark:text-gray-200 hover:text-purple-600">
+                <MdInsights size={24} />
+                <span>Insights</span>
+            </button>
+            <button onClick={onHome} className="flex flex-col items-center text-xs text-gray-700 dark:text-gray-200 hover:text-red-600">
+                <MdHome size={24} />
+                <span>Home</span>
+            </button>
+        </div>
+    );
+}
