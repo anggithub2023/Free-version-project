9ff0f1e6dd08b9e257a6b2ed9eaebbf00e78d87d
ang <alex.ng13@gmail.com>
2025-05-09 17:07:12 -0400

    clean up routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AppShell from './components/AppShell';
 
+// 📄 Pages
 import HomePage from './pages/HomePage';
+import DashboardPage from './pages/DashboardPage';
 import ReflectionPage from './pages/ReflectionPage';
 import ReadinessPage from './pages/ReadinessPage';
 import InjuryPage from './pages/InjuryPage';
@@ -11,27 +13,34 @@ import PlayerStatsPage from './pages/PlayerStatsPage';
 import VideosPage from './pages/VideosPage';
 import WorkoutsPage from './pages/WorkoutsPage';
 import AnalyticsDashboard from './pages/AnalyticsDashboard';
-import DashboardPage from './pages/DashboardPage';
 
-import GetStartedPage from './pages/GetStartedPage';
+import LoginPage from './pages/LoginPage';
 import AuthCallback from './pages/AuthCallback';
-import LoginPage from './pages/LoginPage'; // ✅ Import it
-import CreateEventPage from './pages/CreateEventPage';
+import GetStartedPage from './pages/GetStartedPage';
+
 import RSVPEventsPage from './pages/RSVPEventsPage';
-import CoachEventDashboard from './pages/CoachEventDashboard';
-import JoinTeamPage from './pages/JoinTeamPage';
-import TeamManagementPage from './pages/TeamManagementPage'; // ✅ Add this
-import CreateTeamPage from './pages/CreateTeamPage';
-import useAnonymousUser from './hooks/useAnonymousUser';
+import CreateEventPage from './pages/CreateEventPage';
 import EventDetailPage from './pages/EventDetailPage';
+import CoachEventDashboard from './pages/CoachEventDashboard';
 
-function App() {
-    useAnonymousUser();
+import CreateTeamPage from './pages/CreateTeamPage';
+import JoinTeamPage from './pages/JoinTeamPage';
+import TeamManagementPage from './pages/TeamManagementPage';
 
+import TestSupabase from './pages/TestSupabase'; // ✅ Dev utility
+
+export default function App() {
     return (
         <AppShell>
             <Routes>
+                {/* Public */}
                 <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/auth/callback" element={<AuthCallback />} />
+                <Route path="/get-started" element={<GetStartedPage />} />
+
+                {/* Core */}
+                <Route path="/dashboard" element={<DashboardPage />} />
                 <Route path="/reflect" element={<ReflectionPage />} />
                 <Route path="/readiness" element={<ReadinessPage />} />
                 <Route path="/injury" element={<InjuryPage />} />
@@ -39,24 +48,21 @@ function App() {
                 <Route path="/videos" element={<VideosPage />} />
                 <Route path="/workouts" element={<WorkoutsPage />} />
                 <Route path="/analytics" element={<AnalyticsDashboard />} />
-                <Route path="/dashboard" element={<DashboardPage />} />
 
                 {/* Scheduling */}
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/scheduling/events/create" element={<CreateEventPage />} />
                 <Route path="/scheduling/events" element={<RSVPEventsPage />} />
+                <Route path="/scheduling/events/create" element={<CreateEventPage />} />
+                <Route path="/scheduling/events/:id" element={<EventDetailPage />} />
                 <Route path="/scheduling/coach" element={<CoachEventDashboard />} />
 
-                {/* Auth + Teams */}
-                <Route path="/scheduling/events/:id" element={<EventDetailPage />} />
-                <Route path="/get-started" element={<GetStartedPage />} />
-                <Route path="/auth/callback" element={<AuthCallback />} />
-                <Route path="/create-team" element={<CreateTeamPage />} />
-                <Route path="/join-team" element={<JoinTeamPage />} />
-                <Route path="/team-management" element={<TeamManagementPage />} /> {/* ✅ New Route */}
+                {/* Teams */}
+                <Route path="/team/create" element={<CreateTeamPage />} />
+                <Route path="/team/join" element={<JoinTeamPage />} />
+                <Route path="/team/manage" element={<TeamManagementPage />} />
+
+                {/* Dev/Test */}
+                <Route path="/test" element={<TestSupabase />} />
             </Routes>
         </AppShell>
     );
 }
-
-export default App;
