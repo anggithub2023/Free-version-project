57f593f7d85c37a37be74cd1971c3b3942bb2102
ang <alex.ng13@gmail.com>
2025-05-11 19:36:02 -0400

    routes.js new page

---
+++ src/constants/routes.js
@@ -0,0 +1,11 @@
+// src/constants/routes.js
+export const ROUTES = {
+    COACH_DASHBOARD: '/coach-dashboard',
+    LOGIN: '/login',
+    SIGNUP: '/signup',
+    DASHBOARD: '/dashboard',
+    CREATE_TEAM: '/create-team',
+    VIDEOS: '/videos',
+    RESULTS: '/results',
+    WORKOUTS: '/workouts',
+};
