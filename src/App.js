217ef597362ce6ed8432d81870ec8fc90e7ad204
ang <alex.ng13@gmail.com>
2025-05-12 14:40:43 -0400

    import { Routes, Route } from 'react-router-dom';

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // Pages
 import HomePage from './pages/HomePage';
